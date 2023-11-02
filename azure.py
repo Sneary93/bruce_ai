2be15dcf931fed07e7fe070e409ee707c4ab5c8f
Sneary93 <122059496+Sneary93@users.noreply.github.com>
2023-11-02 11:16:27 -0400

    Update azure.py

--- azure.py
+++ azure.py
@@ -1,15 +1,19 @@
 # azure.py
 # Import necessary libraries
 import streamlit as st
-import openai
-import os
-import music_gen  # Import your music generation module
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import note_seq
 
 # Set up your OpenAI API key
-openai.api_key = os.environ.get('Bruce_Ai_Key')
+# Note: You might want to replace this with the appropriate OpenAI key setup.
+# openai.api_key = os.environ.get('Bruce_Ai_Key')
 
 # Initialize Streamlit
 st.title("Hi! I am Bruce Almighty!")
+
+# Load pre-trained model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained("ai-guru/lakhclean_mmmtrack_4bars_d-2048")
+model = AutoModelForCausalLM.from_pretrained("ai-guru/lakhclean_mmmtrack_4bars_d-2048")
 
 # Add a slider to control the temperature
 temperature = st.slider('Temperature', min_value=0.0, max_value=1.0, value=0.7, step=0.05)
@@ -18,8 +22,26 @@ st.write(f'You selected a temperature of: {temperature}')
 # Create a text input field for user queries
 user_input = st.text_input("Let me generate the perfect music for you and your situation. What mood are you currently in?:")
 
-# Send the user's query to OpenAI GPT-3
+# Generate music when user provides input
 if user_input:
-    # Replace OpenAI GPT-3 call with your music generation function
-    generated_music = music_gen.generate_music(user_input, temperature)
-    st.write(generated_music)
+    # Encode the conditioning tokens.
+    generated_sequence = "PIECE_START"
+    input_ids = tokenizer.encode(generated_sequence, return_tensors="pt")
+
+    # Generate more tokens.
+    eos_token_id = tokenizer.encode("TRACK_END")[0]
+    generated_ids = model.generate(
+        input_ids,
+        max_length=2048,
+        do_sample=True,
+        temperature=temperature,
+        eos_token_id=eos_token_id,
+    )
+    generated_sequence = tokenizer.decode(generated_ids[0])
+
+    # Convert generated tokens to NoteSequence
+    note_sequence = token_sequence_to_note_sequence(generated_sequence)
+
+    # Play the generated music
+    synth = note_seq.fluidsynth
+    st.audio(note_seq.play_sequence(note_sequence, synth), format="audio/midi")
